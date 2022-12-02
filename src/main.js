bd0312b941fa611d1aa93771f3d65933d0ac63e6
Mihai Tincu <mihai.tincu.10@gmail.com>
2022-12-02 10:12:50 -0800

    add store

--- src/main.js
+++ src/main.js
@@ -1,7 +1,37 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createStore } from 'vuex'
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css"
 
-createApp(App).mount('#app')
+//import store from './store';
+
+const testData = [];
+
+for (let i = 1; i <= 10; i++) {
+    testData.push({
+        id: i, name: `Product #${i}`, category: `Category ${i % 3}`,
+        description: `This is Product #${i}`, price: i * 50
+    })
+}
+
+const store = createStore({
+    state () {
+      return {
+        products: testData
+      }
+    },
+    mutations: {
+      increment (state) {
+        state.count++
+      }
+    }
+  })
+
+const app = createApp(App)
+
+app.use(store)
+//app.use(store)
+
+app.mount('#app')
