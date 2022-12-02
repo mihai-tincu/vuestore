429f29e929bd3b3cd3d2a226614db648dce52b48
Mihai Tincu <mihai.tincu.10@gmail.com>
2022-12-02 10:55:03 -0800

    pagination

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -3,22 +3,27 @@
         <div v-for="p in products" v-bind:key="p.id" class="card m-1 p-1 bg-light">
             <h4>
                 {{ p.name }}
-                <span class="badge badge-pill badge-primary float-right text-black">
+                <span class="badge badge-pill badge-primary float-right badge-color">
                     {{ formatCurrency(p.price) }}
                 </span>
             </h4>
             <div class="card-text bg-white p-1">{{ p.description }}</div>
         </div>
+        <page-controls />
     </div>
 </template>
 <script>
 import { mapState } from "vuex";
+import { mapGetters} from "vuex";
+import PageControls from "./PageControls";
 
 const formatter = new Intl.NumberFormat('en-US', {style: 'currency', currency: 'USD'})
 
 export default {
+    components: { PageControls },
     computed: {
-        ...mapState(["products"])
+        ...mapState(["products"]),
+        ...mapGetters({ products: "processedProducts" })
     },
     methods: {
         formatCurrency(value) {
@@ -27,3 +32,9 @@ export default {
     }
 }
 </script>
+
+<style>
+.badge-color {
+    background-color: #007bff;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -15,20 +15,36 @@ for (let i = 1; i <= 10; i++) {
 }
 
 const store = createStore({
-    state () {
-      return {
-        products: testData
-      }
+    state() {
+        return {
+            products: testData,
+            productsTotal: testData.length,
+            currentPage: 1,
+            pageSize: 4
+        }
+    },
+    getters: {
+        processedProducts: state => {
+            let index = (state.currentPage - 1) * state.pageSize;
+            return state.products.slice(index, index + state.pageSize);
+        },
+        pageCount: state => Math.ceil(state.productsTotal / state.pageSize)
     },
     mutations: {
-      increment (state) {
-        state.count++
-      }
+        setCurrentPage(state, page) {
+            state.currentPage = page;
+        },
+        setPageSize(state, size) {
+            state.pageSize = size;
+            state.currentPage = 1;
+        }
     }
-  })
+})
 
 const app = createApp(App)
 
 app.use(store)
 
 app.mount('#app')
+
+
